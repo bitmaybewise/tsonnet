e7b8c26cd143f385e55a4b3964cd4ce39997d4aa
Hercules Merscher <hlmerscher@gmail.com>
2024-12-28 23:17:19 +0100

    parsing int literal

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+let usage_msg = "tsonnet <file1> [<file2>] ..."
+let input_files = ref []
+let anonymous_fun filename = input_files := filename :: !input_files
+let spec_list = []
+
+let run_parser filename =
+  let input_channel = open_in filename in
+  let content = really_input_string input_channel (in_channel_length input_channel) in
+  close_in input_channel;
+  let ast = Tsonnet.parse content in
+  Tsonnet.print ast
+
+let () =
+  Arg.parse spec_list anonymous_fun usage_msg;
+  List.iter run_parser !input_files
---
+++ lib/tsonnet.ml
@@ -0,0 +1,10 @@
+open Ast
+
+(** [parse s] parses [s] into an AST. *)
+let parse (s: string) : expr =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.prog Lexer.read lexbuf in
+  ast
+
+let print ast = match ast with
+  | Int i -> Printf.printf "Int %d\n" i
