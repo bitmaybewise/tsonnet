8b1726be79c1a34180d7c5f7dfa882eb67da2e20
Hercules Merscher <hlmerscher@gmail.com>
2025-07-06 12:51:06 +0200

    typing arrays

--- lib/type.ml
+++ lib/type.ml
@@ -8,8 +8,9 @@ type tsonnet_type =
   | Tbool
   | Tnumber
   | Tstring
-  | Tobject of (string * tsonnet_type) list
+  | Tany
   | Tarray of tsonnet_type
+  | Tobject of (string * tsonnet_type) list
   | Lazy of expr
 
 let translate_late_binding translate_fun = fun venv expr ->
@@ -27,6 +28,32 @@ let rec translate expr venv =
     Env.find_var varname venv
       ~succ:(translate_late_binding translate)
       ~err:(Error.error_at pos)
+  | Array (_pos, elems) ->
+    (* As of now, we compare each element and if all have the same type,
+      it is an array of this type, otherwise it will be an array of any.
+      Since JSON doesn't care about types, this is the simpler way of
+      handling such case in compatibility with Jsonnet. However, when we
+      actually use the items, given the flexibility of JSON and Jsonnet
+      regarding types, we may need to support polymorphic arrays in a more
+      flexible way, but I'm leaving this problem unsolved for now until I
+      finally need to.
+    *)
+    (match elems with
+    | [] -> ok (venv, Tany)
+    | elem :: rest ->
+      let* (venv, ty) = translate elem venv in
+      let* (venv, ty) =
+        List.fold_left
+          (fun acc elem -> acc >>= fun (venv, ty) ->
+            let* (venv', elem_ty) = translate elem venv in
+            if ty = elem_ty
+            then ok (venv', elem_ty)
+            else ok (venv', Tany)
+          )
+          (ok (venv, ty))
+          rest
+      in ok (venv, Tarray ty)
+    )
   | Local (_, vars) ->
     let venv' =
       (List.fold_left
